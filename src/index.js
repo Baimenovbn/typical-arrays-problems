7f1f16b42ee3364b6a665ec669a86f846d650f94
baimenovbn <baibn@mail.ru>
2020-10-02 21:19:11 +0600

    feat: add max, min, avg funcs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+
+  let minNum = array[0];
+  for (let currentNum of array) {
+      if (currentNum < minNum) {
+        minNum = currentNum
+      }
+  }
+
+  return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || !array.length) return 0;
+
+    let maxNum = array[0];
+    for (let currentNum of array) {
+        if (currentNum > maxNum) {
+          maxNum = currentNum
+        }
+    }
+  
+    return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || !array.length) return 0;
+
+  let sum = 0;
+  for (let currentNum of array) {
+      sum += currentNum;
+  }
+
+  return sum / array.length;
 }
